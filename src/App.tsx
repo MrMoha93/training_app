fdea400376ece38d345adb8e7ae3774958295af7
MohaK <mohakhalili@outlook.com>
2025-04-06 19:59:26 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div data-theme="light">
+      <h1 className="text-red-500">Hejsan</h1>
+      <button className="btn btn-primary">Daisyui</button>
+    </div>
+  );
+}

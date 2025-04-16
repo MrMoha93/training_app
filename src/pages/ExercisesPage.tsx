54d46f84fa4458841a57d1d69d01f95a8f0ac652
MohaK <mohakhalili@outlook.com>
2025-04-16 18:35:44 +0200

    fix: supporting create and update in handlesave

--- src/pages/ExercisesPage.tsx
+++ src/pages/ExercisesPage.tsx
@@ -7,17 +7,28 @@ export default function ExercisesPage() {
   const [exercises, setExercises] = useState<Exercise[]>([]);
 
   useEffect(() => {
-    const exercises = getExercises();
+    const exercises = [...getExercises()];
     setExercises(exercises);
-  });
+  }, []);
 
-  function handleSave() {
-    const exercises = getExercises();
-    setExercises([...exercises]);
+  function handleSave(exercise: Exercise) {
+    const existingExercise = exercises.find((e) => e.id === exercise.id);
+
+    if (existingExercise) {
+      const updatedExercises = exercises.map((e) => {
+        if (e.id === exercise.id) return exercise;
+        return e;
+      });
+
+      setExercises(updatedExercises);
+    } else {
+      setExercises([...exercises, exercise]);
+    }
   }
 
   return (
     <>
+      <h1 className="text-2xl font-semibold text-center py-10">Training App</h1>
       <div className="flex justify-center">
         <ExerciseModal onSave={handleSave} />
       </div>

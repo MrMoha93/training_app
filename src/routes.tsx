80336bae8611726e9929644ab06df16e93fcaa45
MohaK <mohakhalili@outlook.com>
2025-04-27 17:12:02 +0200

    removed the sessions

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import ExercisePage from "./pages/ExercisePage";
 import ExercisesPage from "./pages/ExercisesPage";
-import TrainingSessionPage from "./pages/TrainingSessionPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { path: "sessions", element: <TrainingSessionPage /> },
+      { index: true, element: <ExercisesPage /> },
       { path: "exercises", element: <ExercisesPage /> },
       { path: "exercises/:id", element: <ExercisePage /> },
     ],

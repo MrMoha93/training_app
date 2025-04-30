1f075255460b69ccbfbce1e1fabfd87fb397b1ad
MohaK <mohakhalili@outlook.com>
2025-05-01 00:50:08 +0200

    image upload with validation and cloudinary integration

--- src/components/ExerciseModal.tsx
+++ src/components/ExerciseModal.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Exercise, saveExercise } from "../services/ExerciseService";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import cloudinaryService from "../services/cloudinaryService";
 
 const schema = z.object({
   name: z.string().min(1, { message: "Name is required" }),
@@ -14,6 +15,25 @@ const schema = z.object({
   reps: z.coerce
     .number({ invalid_type_error: "Reps must be a number" })
     .gte(1, { message: "Reps must be at least 1" }),
+  images: z
+    .instanceof(FileList)
+    .refine(
+      (fileList) => {
+        if (!fileList || fileList.length === 0) return true;
+        return ["image/png", "image/jpg"].includes(fileList[0].type);
+      },
+      { message: "Only PNG/JPG images are allowed" }
+    )
+    .refine(
+      (fileList) => {
+        if (!fileList || fileList.length === 0) return true;
+        return fileList[0].size <= 5_000_000;
+      },
+      {
+        message: "Maximum file size allowed is 5 MB",
+      }
+    )
+    .optional(),
 });
 
 type FormData = z.infer<typeof schema>;
@@ -49,10 +69,17 @@ export default function ExerciseModal({
   }, [onReset, reset]);
 
   async function onSubmit(data: FormData, goToExercise = false) {
+    let uploadedImageUrl: string | undefined = undefined;
+
+    if (data.images && data.images.length > 0) {
+      uploadedImageUrl = await cloudinaryService.saveImage(data.images[0]);
+    }
+
     const exerciseData = {
       id: selectedExercise?.id,
       name: data.name,
       date: data.date,
+      imageUrl: uploadedImageUrl,
       sets: [
         {
           weight: data.weight,
@@ -136,10 +163,31 @@ export default function ExerciseModal({
             className="block input mt-4"
           />
           {errors.date && <p className="text-error">{errors.date.message}</p>}
-          <button className="btn btn-secondary mt-2">Save</button>
+          {selectedExercise?.imageUrl && (
+            <div className="self-start mt-2">
+              <img
+                src={selectedExercise.imageUrl}
+                alt={selectedExercise.name}
+                width={80}
+              />
+            </div>
+          )}
+          {selectedExercise?.imageUrl ? (
+            <p className="text-sm mb-1">Change Image (Optional):</p>
+          ) : (
+            <p className="text-sm mt-2">Upload Image (Optional):</p>
+          )}
+          <div className="w-full flex justify-center mt-4">
+            <input
+              type="file"
+              {...register("images")}
+              className="file-input file-input-bordered"
+            />
+          </div>
+          <button className="btn btn-secondary mt-4">Save</button>
           <button
             type="button"
-            className="btn btn-secondary mt-2 ml-2"
+            className="btn btn-secondary mt-4 ml-2"
             onClick={handleSaveAndOpen}
           >
             Save & Open

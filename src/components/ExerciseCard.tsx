6fb5b7315a46e1bacbcc672ba1af73c7f7323383
MohaK <mohakhalili@outlook.com>
2025-04-30 16:59:57 +0200

    showing static image in exercise card

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -18,24 +18,29 @@ export default function ExerciseCard({ exercises, modalRef, onSelect }: Props) {
     <div className="container mx-auto p-5 grid place-items-center gap-5">
       {exercises.map((exercise) => (
         <div
+          key={exercise.id}
           className="relative card bg-base-100 image-full w-96 shadow-sm cursor-pointer"
           onClick={() => navigate(`/exercises/${exercise.id}`)}
         >
           <figure>
             <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-              alt={exercise.name}
+              src="/images/exercise.png"
+              alt="Product image"
+              style={{ width: 100, height: 150 }}
             />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">{exercise.name}</h2>
+            <h2 className="card-title text-primary">{exercise.name}</h2>
             <i
-              className="absolute top-2 right-2 fas fa-pen cursor-pointer"
+              className="absolute top-2 right-2 fas fa-pen cursor-pointer text-secondary"
               onClick={(e) => {
                 e.stopPropagation();
                 handleSelectExercise(exercise);
               }}
             />
+            <h2 className="card-title text-secondary">
+              {exercise.date.split("T")[0]}
+            </h2>
           </div>
         </div>
       ))}
